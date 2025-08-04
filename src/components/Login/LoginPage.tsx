0bd606a980f1614734382b51f623fc68d3ce5600
soliha10 <toxtamurodovagulshodaa@gmail.com>
2025-08-04 12:34:29 +0500

    Make validation in form

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -6,8 +6,36 @@ import lock from '../../assets/enter-lofin.svg';
 
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object({
+	email: yup.string().email('Неверный формат').required('Обязательное поле'),
+	password: yup
+		.string()
+		.min(3, 'Минимум 3 символа')
+		.required('Обязательное поле'),
+});
+
 export default function LoginPage() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+		mode: 'all',
+		resolver: yupResolver(schema),
+	});
+	const router = useNavigate();
+	const onSubmit = () => {
+		router('/all-order');
+	};
 	return (
 		<div className='flex items-start'>
 			<img className='me-[100px] ' src={cafe} alt='cafe' />
@@ -18,37 +46,49 @@ export default function LoginPage() {
 				<h1 className='text-[#2F3138] text-2xl font-semibold mb-12 text-center'>
 					Вход в аккаунт
 				</h1>
-				<form action=''>
+				<form action='' onSubmit={handleSubmit(onSubmit)}>
 					<div className='relative w-full mb-4'>
 						<Input
 							className='border-[#ECEDEE] rounded-[6px] ps-10 placeholder:text-[#7D848B] text-[14px] w-full bg-white'
 							placeholder='Логин'
+							{...register('email')}
 						/>
 						<img
 							src={user}
 							alt=''
 							className='absolute left-[14px] top-1/2 transform -translate-y-1/2 w-[15px] h-4'
 						/>
+						<span className='text-red-500 absolute bottom-[-15px] left-1 text-xs'>
+							{errors.email?.message}
+						</span>
 					</div>
 					<div className='relative w-full mb-6'>
 						<Input
 							className='border-[#ECEDEE] rounded-[6px] ps-10 w-full bg-white'
 							placeholder='Пароль'
 							type='password'
+							{...register('password')}
 						/>
 						<img
 							src={lock}
 							alt=''
 							className='absolute left-[14px] top-1/2 transform -translate-y-1/2 w-[15px] h-4'
 						/>
+						<span className='text-red-500 absolute bottom-[-15px] left-1 text-xs'>
+							{errors.password?.message}
+						</span>
 					</div>
-          <Link to='/all-order'>
-          
-					<Button className='bg-[#56F] w-full py-[10px] px-3 text-white text-[14px] font-medium cursor-pointer rounded-[5px]  '>
+
+					<Button
+						disabled={!isValid}
+						type='submit'
+						className='bg-[#56F] w-full py-[10px] px-3 text-white text-[14px] font-medium cursor-pointer rounded-[5px]  '
+					>
+						{/* <Link className='flex items-center' to='/all-order'> */}
 						<img src={enter} alt='' />
 						Войти в аккаунт
+						{/* </Link> */}
 					</Button>
-          </Link>
 				</form>
 			</div>
 		</div>
