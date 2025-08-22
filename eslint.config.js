8b6089058e41d9294da322817b9ef3cb33caceee
soliha10 <toxtamurodovagulshodaa@gmail.com>
2025-08-22 12:43:29 +0500

    Fix form component error

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,7 @@ export default tseslint.config([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      // reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
